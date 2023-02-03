cf6b1c3480c6325e9ce10837fd9524e79a3fe0d0
nyeliep <gielpalnyeliep@gmail.com>
2023-02-03 08:13:00 +0300

    my first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    var fullName = "Nyeliep Nial"
+    println(fullName)
+
+    var age = 21
+    println(age)
+
+    var phoneNumber = "0113704422"
+    println(phoneNumber)
+
+    var weightInKg = 49
+    println(weightInKg)
+
+    var kenyanCitizen: Boolean = false
+    println(kenyanCitizen)
+
+
+
+}
+
